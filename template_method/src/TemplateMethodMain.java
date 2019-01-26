54c871fd6fdcc5286771a95b8acee0d2f72a6307
dzazef <dzazef@gmail.com>
2019-01-26 17:15:10 +0100

    Add template method

--- template_method/src/TemplateMethodMain.java
+++ template_method/src/TemplateMethodMain.java
@@ -1,17 +1,58 @@
+abstract class Advertisement {
+    abstract void translate(String language);
+    abstract void send();
+    void deliver(String language) {
+        translate(language);
+        send();
+    }
+}
 
+class SMSAdvertisement extends Advertisement {
 
+    @Override
+    void translate(String language) {
+        System.out.println("Tłumaczę na "+language);
+    }
 
+    @Override
+    void send() {
+        System.out.println("Wysyłam za pośrednictwem SMS");
+    }
+}
 
+class MailAdvertisement extends Advertisement {
 
+    @Override
+    void translate(String language) {
+        System.out.println("Tłumaczę na "+language);
+    }
 
+    @Override
+    void send() {
+        System.out.println("Wysyłam za pośrednictwem email");
+    }
+}
 
+class VoiceAdvertisement extends Advertisement {
 
+    @Override
+    void translate(String language) {
+        System.out.println("Tłumaczę na "+language);
+    }
 
-
-
+    @Override
+    void send() {
+        System.out.println("Wysyłam za pośrednictwem wiadomości głosowej");
+    }
+}
 
 public class TemplateMethodMain {
     public static void main(String[] args) {
-
+        Advertisement advertisement = new VoiceAdvertisement();
+        advertisement.deliver("ENGLISH");
+        advertisement = new SMSAdvertisement();
+        advertisement.deliver("SPANISH");
+        advertisement = new MailAdvertisement();
+        advertisement.deliver("SUAHILI");
     }
 }
